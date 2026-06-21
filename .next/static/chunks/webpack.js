b4ea8d588adceacb8c12729deb0ddf560f7eafd7
GerarTech <97939769+GerarTech@users.noreply.github.com>
2026-06-21 20:59:45 +0300

    nj

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd8f5984b9629159"; }
+/******/ 		__webpack_require__.h = function() { return "0cb2bde290a454c7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
